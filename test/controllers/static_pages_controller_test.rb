d8c4058323ddc654cd980b3d8ad9c1d09d0b1301
unprevailing <unprevailing@gmail.com>
2015-08-28 10:27:44 -0600

    Eliminated repetition (sort of)

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,22 +1,33 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+
+	 def setup
+    @base_title = "Twitter Clone"
+  end
+
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | Twitter Clone"
+    assert_select "title", "Home | #{@base_title}"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "Help | Twitter Clone"
+    assert_select "title", "Help | #{@base_title}"
   end
 
   test "should get about" do
   	get :about
   	assert_response :success
-  	assert_select "title", "About | Twitter Clone"
+  	assert_select "title", "About | #{@base_title}"
+  end
+
+    test "should get contact" do
+  	get :contact
+  	assert_response :success
+  	assert_select "title", "Contact | #{@base_title}"
   end
 
 end
